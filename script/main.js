b3819cd41e1b9b4e147149f0a21c0774f04fa256
Md-Shakib-Akando <shakibakando2@gmail.com>
2025-02-28 23:37:36 +0600

    navbar style and using js

---
+++ script/main.js
@@ -0,0 +1,13 @@
+
+const body = document.body;
+
+const colors=['rgb(244, 247, 255)','rgb(204, 255, 219)','rgb(204,255, 255)', 'rgb(255,204, 255)','rgb(255,204, 204)','rgb(255,204, 229)']
+
+body.style.backgroundColor='rgb(244, 247, 255)';
+
+document.getElementById("colorBtn").addEventListener("click",function(){
+
+    const colorIndex=Math.floor(Math.random()*colors.length)
+    body.style.backgroundColor=colors[colorIndex];
+})
+
